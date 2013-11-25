16fa0f7f7bfadadc5da556604db8c22f1198b9ca
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 15:38:57 +0800

    ABC

--- ai.js
+++ ai.js
@@ -43,6 +43,46 @@ function aiAction(){
 	//destPos : the position of the ai grid 
 	return {focusCardIndex:choose,destPos:destPos};*/
 }
+function random(){
+	//thoughts by jimmyshum:
+	//ai player would choose the cards in random
+
+	var randNum;
+
+	//For the CardDeck Part
+	//to ensure that the random num is within 0 - 5 (6 will cause arrayOutOfBound)
+	do{
+	 	randNum = Math.floor(Math.random()*6);
+
+	 	//|check
+	 	//alert("choose: "+randNum);
+	 	//|check
+	}while(randNum == 6);
+	var choose = randNum;
+
+	//For the ai grid part
+	//to ensure that the random num is within 0 - 5 (6 will cause arrayOutOfBound)
+	do{
+		do{
+			randNum = Math.floor(Math.random()*9);	
+		}while(randNum == 9);
+		//|check
+		//alert("destPos: "+randNum);
+		//|check
+		if (!ai.grid[randNum]) {
+			var destPos = {pos:randNum,x:(randNum%3)*105+ai.gridPosX,y:Math.floor(randNum/3)*105+170};
+		}
+		//|check
+		//alert(!ai.grid[randNum]);
+		//|check
+	}while(ai.grid[randNum]);
+
+	//focusCardIndex : the index from the card deck 
+	//destPos : the position of the ai grid 
+	return {focusCardIndex:choose,destPos:destPos};
+
+
+}
 function aggressive(){
 	//thoughts by jimmyshum:
 	//ai player would not only be target-oriented, it would also think about the player grid for raising the probability of winning the game
