86f9d556eddcf5c505b480d24dae98d31099a31e
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 17:18:24 +0800

    A

--- ai.js
+++ ai.js
@@ -85,7 +85,7 @@ function conservative(){
 		{
 			if ( ai.grid[i]  &&  dealtCards[j].rank==ai.grid[i].rank) {				// Get for pairs && ai.grid[i] : check whether the grid has card or not 			
 					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
-					var found=j;
+				//	var found=j;
 					return {focusCardIndex:choose,destPos:destPos};
 			}
 			/*
