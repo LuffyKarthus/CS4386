e9697385ab686380983c228027ae94f90bf541f1
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 15:09:49 +0800

    ABC

--- ai.js
+++ ai.js
@@ -82,18 +82,18 @@ function conservative(){
 		{
 			if (!ai.grid[i] && ai.grid[i+3]%8 && dealtCards[j].rank==ai.grid[i].rank ){				// Get for pairs && ai.grid[i] : check whether the grid has card or not 		
 				
-					var deskPos={pos:i,x:((i)%3)*105+ai.gridPosX,y:Math.floor((i)/3)*105+170};
+					destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
 						return {focusCardIndex:pick[z],destPos:destPos};
 			}
 			if ( (!ai.grid[i] && ai.grid[i+3]%8 &&  ((dealtCards[j].rank-1>ai.grid[i].rank) || (dealtCards[j].rank+1>ai.grid[i].rank) ) )){					// Get for Straight && ai.grid[i] : check whether the grid has card or not
 				{
-					var deskPos={pos:i,x:((i)%3)*105+ai.gridPosX,y:Math.floor((i)/3)*105+170};
+					destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
 					return {focusCardIndex:pick[z],destPos:destPos};
 				}																	
 			}
 			if (!ai.grid[i] && ai.grid[i+3]%8 && dealtCards[j].suit==ai.grid[i].suit){					// Get for Flush && ai.grid[i] : check whether the grid has card or not
 				{
-					var deskPos={pos:i,x:((i)%3)*105+ai.gridPosX,y:Math.floor((i)/3)*105+170};
+					destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
 						return {focusCardIndex:pick[z],destPos:destPos};
 				}																
 			}
