042f90246c77ebde6fe2a2c371526ba8021f6a3e
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-26 00:39:08 +0800

    ABCS

--- ai.js
+++ ai.js
@@ -52,7 +52,7 @@ function conservative(){			//By Karthus
 
 	for (var i = 0; i < 9; i++){
 		if (!ai.grid[i]) {										//ai.grid[i] : check whether the grid has card or not 
-			var destPos = {pos:i,x:((i+1)%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};	
+			var destPos = {pos:i,x:((i+0)%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};	
 			break;
 		}
 	}
@@ -64,7 +64,7 @@ function conservative(){			//By Karthus
 					found=j;
 					alert("found");	
 					if (ai.grid[(i)] && ai.grid [(i+1)%8] ){
-						var destPos = {pos:i,x:((i+1)%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
+						var destPos = {pos:i,x:((i+0)%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
 						alert("check empty grid 1");
 						return {focusCardIndex:found,destPos:destPos};		
 					}	
