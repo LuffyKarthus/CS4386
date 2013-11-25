497a338ab159e0066e0ea727a6b143dfcb2dddfd
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 17:05:53 +0800

    ABC

--- ai.js
+++ ai.js
@@ -47,6 +47,70 @@ function aiAction(){
 	return {focusCardIndex:choose,destPos:destPos};*/
 }
 
+
+function conservative(){
+	//thoughts by jimmyshum:
+	//ai tends to get points for completing any conditions
+	/*
+	var pick=new Array();
+	var z=0;
+
+	for (var i=0;i<6;i++)									// not picking up the special cards
+	{
+			if (dealtCards[i].suit!=SPECIAL_SUIT)
+			{
+				pick[z]=i;
+				z++;
+				break;
+			}
+	}
+	*/
+	for (var i = 0; i < 6; i++)
+
+		//suit 0 - 3 => spade heart diamond club
+		//suit 4 => special card
+
+		if (dealtCards[i].suit != SPECIAL_SUIT ) {
+			var choose = i;
+			break;
+		}
+
+	for (var i = 0; i < 9; i++)		
+		if (!ai.grid[i]) {												//ai.grid[i] : check whether the grid has card or not 
+			var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};		
+			break;
+		}
+
+	for (var i=0;i<9;i++)					//Priority from getting pairs,Straight,Flush.    If a pattern can't be formed  ,then simply pick a card
+	{
+		for (var j=0;j<6;j++)
+		{
+			if ( ai.grid[i]  &&  dealtCards[j].rank==ai.grid[i].rank) {				// Get for pairs && ai.grid[i] : check whether the grid has card or not 			
+					var destPos = {pos:i,x:((i+3)%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
+					var found=j;
+					return {focusCardIndex:found,destPos:destPos};
+			}
+			/*
+			if ( (!ai.grid[i]  &&  ((dealtCards[j].rank-1>ai.grid[i].rank) || (dealtCards[j].rank+1>ai.grid[i].rank) ) )){		// Get for Straight && ai.grid[i] : check whether the grid has card or not
+					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
+					var found=j;
+					return {focusCardIndex:found,destPos:destPos};
+																
+			}		//	&& ai.grid[i+3]%8
+			if (!ai.grid[i]  && (dealtCards[j].suit==ai.grid[i].suit)){					// Get for Flush && ai.grid[i] : check whether the grid has card or not
+					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
+					var found=j;
+					return {focusCardIndex:found,destPos:destPos};															
+			}
+			*/
+		}	
+	}
+
+	alert("hi");
+			return {focusCardIndex:choose,destPos:destPos};							//focusCardIndex : the index from the card deck 
+																					//destPos : the position of the ai grid 
+}
+
 function aggressive(){
 	//thoughts by jimmyshum:
 	//ai player would not only be target-oriented, it would also think about the player grid for raising the probability of winning the game
@@ -102,66 +166,3 @@ function targetOriented(){
 		return random();
 }
 
-function conservative(){
-	//thoughts by jimmyshum:
-	//ai tends to get points for completing any conditions
-	/*
-	var pick=new Array();
-	var z=0;
-
-	for (var i=0;i<6;i++)									// not picking up the special cards
-	{
-			if (dealtCards[i].suit!=SPECIAL_SUIT)
-			{
-				pick[z]=i;
-				z++;
-				break;
-			}
-	}
-	*/
-	for (var i = 0; i < 6; i++)
-
-		//suit 0 - 3 => spade heart diamond club
-		//suit 4 => special card
-
-		if (dealtCards[i].suit != SPECIAL_SUIT ) {
-			var choose = i;
-			break;
-		}
-
-	for (var i = 0; i < 9; i++)		
-		if (!ai.grid[i]) {												//ai.grid[i] : check whether the grid has card or not 
-			var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};		
-			break;
-		}
-
-	for (var i=0;i<9;i++)					//Priority from getting pairs,Straight,Flush.    If a pattern can't be formed  ,then simply pick a card
-	{
-		for (var j=0;j<6;j++)
-		{
-			if ( ai.grid[i]  &&  dealtCards[j].rank==ai.grid[i].rank) {				// Get for pairs && ai.grid[i] : check whether the grid has card or not 			
-					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
-					var found=j;
-					return {focusCardIndex:found,destPos:destPos};
-			}
-			/*
-			if ( (!ai.grid[i]  &&  ((dealtCards[j].rank-1>ai.grid[i].rank) || (dealtCards[j].rank+1>ai.grid[i].rank) ) )){		// Get for Straight && ai.grid[i] : check whether the grid has card or not
-					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
-					var found=j;
-					return {focusCardIndex:found,destPos:destPos};
-																
-			}		//	&& ai.grid[i+3]%8
-			if (!ai.grid[i]  && (dealtCards[j].suit==ai.grid[i].suit)){					// Get for Flush && ai.grid[i] : check whether the grid has card or not
-					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
-					var found=j;
-					return {focusCardIndex:found,destPos:destPos};															
-			}
-			*/
-		}	
-	}
-
-	alert("hi");
-			return {focusCardIndex:choose,destPos:destPos};							//focusCardIndex : the index from the card deck 
-																					//destPos : the position of the ai grid 
-
-}
