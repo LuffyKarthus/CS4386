fda668313cc9585dc8bc3e5307562c6f5df0ba5f
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-26 00:22:33 +0800

    ABCS

--- ai.js
+++ ai.js
@@ -75,14 +75,14 @@ function conservative(){			//By Karthus
 					var found=j;
 					alert("found");	
 
-					if (ai.grid[(i)] && ai.grid [	(i+1)%8	] ){
-						var destPos = {pos:i,x:((i+1)%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
+					if (	ai.grid[(i)] && ai.grid [(i+1)%8] 	){
+						var destPos = {pos:i,x:((i+1)%8)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
 						alert("check empty grid 1");
 						return {focusCardIndex:found,destPos:destPos};		
 					}
 					
 			}
-			
+		
 				found=choose;
 			/*		// Get for Straight && ai.grid[i] : check whether the grid has card or not
 			if ( (!ai.grid[i]  &&  ((dealtCards[j].rank-1>ai.grid[i].rank) || (dealtCards[j].rank+1>ai.grid[i].rank) ) )){		
