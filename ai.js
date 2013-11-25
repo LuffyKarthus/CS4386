50e811ec4999e0f1d45a68f914d06277209783d8
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 18:22:50 +0800

    ABC

--- ai.js
+++ ai.js
@@ -93,7 +93,7 @@ function conservative(){			//By Karthus
 						alert("check empty4");
 					}
 */
-					for (var j=0;j<9;j++){
+					for (var j=i;j<9;j++){
 						if (ai.grid[j])
 						var found=j;
 						break;
