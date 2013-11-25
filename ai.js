f37317d4e2932bad4655c963d6f7b3c7fddb6216
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 17:44:15 +0800

    ABC

--- ai.js
+++ ai.js
@@ -79,7 +79,7 @@ function conservative(){			//By Karthus
 					}
 					if (ai.grid[(i+2)%8]==null)
 					{
-						var destPos = {pos:i,x:(i+%3)*105+ai.gridPosX,y:Math.floor((i)/3)*105+170};
+						var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor((i)/3)*105+170};
 						alert("check empty2");
 						alert(destPos);
 					}
