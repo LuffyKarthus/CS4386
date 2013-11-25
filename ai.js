85b18e7a099fb01a085d643732f04ebc5a2d19ca
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 23:57:23 +0800

    ABC

--- ai.js
+++ ai.js
@@ -55,8 +55,6 @@ function conservative(){			//By Karthus
 				break;
 			}
 	}*/
-	var checkempty=new Array();
-
 	for (var i = 0; i < 6; i++)
 		if (dealtCards[i].suit != SPECIAL_SUIT ) {								//suit 0 - 3 => spade heart diamond club										
 			var choose = i;														//suit 4 => special card
@@ -68,7 +66,7 @@ function conservative(){			//By Karthus
 			var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};	
 			break;
 		}
-		checkempty[i]=0;
+
 	}
 	for (var i=0;i<9;i++)		//Priority from getting pairs,Straight,Flush.    If a pattern can't be formed  ,then simply pick a card
 	{
@@ -76,9 +74,8 @@ function conservative(){			//By Karthus
 		{
 			if ( ai.grid[i]  &&  dealtCards[j].rank==ai.grid[i].rank){		// Get for pairs && ai.grid[i] : check whether the grid has card or not 			
 					var found=j;
-					alert("found");
-					checkempty[i]=1;			//	if (checkempty[i]==1) not empty 
-					if (ai.grid[(i)] && checkempty[i]==0){
+					alert("found");			
+					if (ai.grid[(i)] ){
 						var destPos = {pos:i,x:((i+1)%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
 						alert("check empty grid 1");
 						return {focusCardIndex:found,destPos:destPos};
