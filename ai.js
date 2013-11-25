954cf2e830e7f2faafb745033802af7455e45ece
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 19:00:22 +0800

    ABC

--- ai.js
+++ ai.js
@@ -74,7 +74,7 @@ function conservative(){			//By Karthus
 					alert("found");
 					
 
-					if (ai.grid[(i)%8] ){
+					if (ai.grid[(i)%3]){
 						var destPos = {pos:i,x:((i+1)%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
 						alert("check empty grid 1");
 						return {focusCardIndex:found,destPos:destPos};
