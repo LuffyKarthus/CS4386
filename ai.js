ceea3b29912c92ff67e0dfe905d5b6a662912257
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-25 16:10:28 +0800

    ABC

--- ai.js
+++ ai.js
@@ -141,19 +141,19 @@ function conservative(){
 		{
 			if (!ai.grid[i+3] && ai.grid[i+3]%8 && dealtCards[j].rank==ai.grid[i].rank ){				// Get for pairs && ai.grid[i] : check whether the grid has card or not 			
 					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
-					//choose=j;
-					return {focusCardIndex:choose,destPos:destPos};
+					var found=j;
+					return {focusCardIndex:found,destPos:destPos};
 			}
 			if ( (!ai.grid[i+3] && ai.grid[i+3]%8 &&  ((dealtCards[j].rank-1>ai.grid[i].rank) || (dealtCards[j].rank+1>ai.grid[i].rank) ) )){		// Get for Straight && ai.grid[i] : check whether the grid has card or not
 					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
-					//choose=j;
-					return {focusCardIndex:choose,destPos:destPos};
+					var found=j;
+					return {focusCardIndex:found,destPos:destPos};
 																
 			}
 			if (!ai.grid[i+3] && ai.grid[i+3]%8 && dealtCards[j].suit==ai.grid[i].suit){					// Get for Flush && ai.grid[i] : check whether the grid has card or not
 					var destPos = {pos:i,x:(i%3)*105+ai.gridPosX,y:Math.floor(i/3)*105+170};
-					//choose=j;
-					return {focusCardIndex:choose,destPos:destPos};															
+					var found=j;
+					return {focusCardIndex:found,destPos:destPos};															
 			}
 
 		}	
