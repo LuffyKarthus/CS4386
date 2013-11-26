a43f1b2f740c5a2a0c7d0b5a987f86cc9cdd6144
LuffyKarthus <w.shuiyin@yahoo.com>
2013-11-26 18:30:45 +0800

    ABCD

--- ai.js
+++ ai.js
@@ -94,10 +94,10 @@ function conservative()
 		for (var j=0;j<6;j++){
 								if (ai.grid[i] && dealtCards[j].suit==ai.grid[i].suit ){		
 									found=j;
-										if (ai.grid[(i)] && ai.grid [(i+1)%8] && ai.grid[(i+2)%8]){												
+							//			if (ai.grid[(i)] && ai.grid [(i+1)%8] && ai.grid[(i+2)%8]){												
 												for (var z=0;z<3;z++){
 													if (!ai.grid[z]){
-													//	alert("flush");
+														alert("flush");
 														var destPos = {pos:z,x:(z)*105+ai.gridPosX,y:Math.floor(i%3)*105+170};
 														return {focusCardIndex:found,destPos:destPos};	
 													}
@@ -111,10 +111,10 @@ function conservative()
 		for (var j=0;j<6;j++){
 								if ( (ai.grid[i]  &&  ((dealtCards[j].rank-1>ai.grid[i].rank) || (dealtCards[j].rank+1>ai.grid[i].rank) ) )){	 	
 									found=j;
-											if (ai.grid[(i)] && ai.grid [(i+1)%8] && ai.grid[(i+2)%8]){
+									//		if (ai.grid[(i)] && ai.grid [(i+1)%8] && ai.grid[(i+2)%8]){
 													for (var z=0;z<3;z++){
 														if (!ai.grid[z]){
-														//	alert("straight");
+															alert("straight");
 															var destPos = {pos:z,x:(z)*105+ai.gridPosX,y:Math.floor(i%3)*105+170};
 															return {focusCardIndex:found,destPos:destPos};	
 														}
