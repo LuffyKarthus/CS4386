0a80adb2d182ce97980630bb238a4490397c76c9
jimmyshum <jimmyshum2001@gmail.com>
2013-11-26 17:31:43 +0800

    ai.js targetOriented Change 1

--- ai.js
+++ ai.js
@@ -20,13 +20,7 @@ Plz follow the return format of aiAction().
 
 */
 function aiAction(){
-<<<<<<< HEAD
-
-	return conservative();
-
-=======
-	return conservative();
->>>>>>> f7323ec9af26d5df9fe8700cf2926a3c79b95b66
+	return targetOriented();
 	//For refecing
 	/*for (var i = 0; i < 6; i++)
 
@@ -97,6 +91,8 @@ function aggressive(){
 function targetOriented(){
 	//thoughts by jimmyshum:
 	//ai player would like to choose some particular conditions for awarding higher scores 
+	var choose;
+	var destPos;
 	var prior = new Array();
 	var occupiedGrid = 0;
 	//count the empty grid in ai panel
@@ -115,9 +111,14 @@ function targetOriented(){
 				}
 			}
 		}
-		var choose = prior[Math.floor(Math.random()*prior.length)];
+		if(prior.length > 0){
+			choose = prior[Math.floor(Math.random()*prior.length)];
+			prior = new Array();
+		}
+		else
+			return random();
 		//test
-		alert(choose);
+		alert("choose-0: " +choose);
 
 
 		//For the ai grid part
@@ -130,7 +131,7 @@ function targetOriented(){
 			//alert("destPos: "+randNum);
 			//|check
 			if (!ai.grid[randNum]) {
-				var destPos = {pos:randNum,x:(randNum%3)*105+ai.gridPosX,y:Math.floor(randNum/3)*105+170};
+				destPos = {pos:randNum,x:(randNum%3)*105+ai.gridPosX,y:Math.floor(randNum/3)*105+170};
 			}
 			//|check
 			//alert(!ai.grid[randNum]);
@@ -140,8 +141,282 @@ function targetOriented(){
 		return {focusCardIndex:choose,destPos:destPos};
 
 	}
-	else
-		return random();
+	else{
+		var valueArr = checkValue(dealtCards,ai.grid);
+		var maxValueInValueArr = new Array();
+		for(var i=0;i<6;i++){
+			maxValueInValueArr.push(calMaxValue(valueArr[i]));
+		}
+		var highest = calMaxValue(maxValueInValueArr);
+		for(var i=0;i<6;i++){
+			if(maxValueInValueArr[i] == highest){
+				prior.push(i);
+			}
+		}
+		choose = prior[Math.floor(Math.random()*prior.length)];
+		prior = new Array();
+
+
+
+		highest = calMaxValue(valueArr[choose]);
+		for(var i=0;i<9;i++){
+			if(valueArr[choose][i] == highest){
+				prior.push(i);
+			}
+		}
+		var p = prior[Math.floor(Math.random()*prior.length)];
+		if(!ai.grid[p]){
+			destPos = {pos:p,x:(p%3)*105+ai.gridPosX,y:Math.floor(p/3)*105+170};
+		}
+		prior = new Array();
+
+		//test
+		alert("choose: " + choose + "  destPos: " + p );
+
+
+		return {focusCardIndex:choose,destPos:destPos};
+
+
+
+
+
+	}
+	return random();
+
+}
+
+
+function max(num1,num2,num3,num4){
+
+	//check
+	//alert(num1 + " " + num2 + " " + num3 + " " + num4);
+	//
+
+	var highest = num1;
+	if(num2 > highest){
+		highest = num2;
+	}
+	if(num3 > highest){
+		highest = num3;
+	}
+	if(num4 > highest)
+		highest = num4;
+	return highest;
+}
+function analysisCards(dealtCard,card1,card2){
+	var c1 = -1;
+	var c2 = -1;
+	//for the case of no card inside the grid
+	if(!card1)
+		c1 = 0;
+	if(!card2)
+		c2 = 0;
+
+	//for dealtCard only
+	if(c1 == 0 && c2 == 0){
+		//no comparison
+		return 1;
+
+	}
+	//for one 
+	if(c1 == 0){
+		if(dealtCard.rank >= 10 && dealtCard.rank <= 12 && card2.rank >= 10 && card2.rank <= 12){ //JQK 
+			if(dealtCard.suit == card2.suit && dealtCard.rank != card2.rank){ //JQK Straight Flush
+				return 6;
+			}
+			else{
+				if(dealtCard.rank - card2.rank >= -2 && dealtCard.rank - card2.rank <= 2 && dealtCard.rank != card2.rank){  //JQK Straight
+					return 3;
+				}
+				else if(dealtCard.rank - card2.rank == 0){ //JQK same kind
+					return 2;
+				}
+			}
+		}
+		else{
+			
+			if(dealtCard.suit == card2.suit){ //not JQK
+				if(dealtCard.rank - card2.rank >= -2 && dealtCard.rank - card2.rank <= 2 && dealtCard.rank != card2.rank){ //Straight Flush
+					return 5;
+				}
+				else	//Flush
+					return 4;
+			}
+			else{
+				if(dealtCard.rank - card2.rank >= -2 && dealtCard.rank - card2.rank <= 2 && dealtCard.rank != card2.rank){ //Straight
+					return 3;
+				}
+				else if(dealtCard.rank - card2.rank == 0){ //Same kind
+					return 2;
+				}
+				else //can't form these kind above
+					return 0;
+			}
+			
+
+		}
+
+	}
+
+	if(c2 == 0){
+		if(dealtCard.rank >= 10 && dealtCard.rank <= 12 && card1.rank >= 10 && card1.rank <= 12){ //JQK 
+			if(dealtCard.suit == card1.suit && dealtCard.rank != card1.rank){ //JQK Straight Flush
+				return 6;
+			}
+			else{
+				if(dealtCard.rank - card1.rank >= -2 && dealtCard.rank - card1.rank <= 2 && dealtCard.rank != card1.rank){  //JQK Straight
+					return 3;
+				}
+				else if(dealtCard.rank - card1.rank == 0){ //JQK same kind
+					return 2;
+				}
+			}
+		}
+		else{
+			if(dealtCard.suit == card1.suit){ //not JQK
+				if(dealtCard.rank - card1.rank >= -2 && dealtCard.rank - card1.rank <= 2 && dealtCard.rank != card1.rank){ //Straight Flush
+					return 5;
+				}
+				else	//Flush
+					return 4;
+			}
+			else{
+				if(dealtCard.rank - card1.rank >= -2 && dealtCard.rank - card1.rank <= 2 && dealtCard.rank != card1.rank){ //Straight
+					return 3;
+				}
+				else if(dealtCard.rank - card1.rank == 0){ //Same kind
+					return 2;
+				}
+				else //can't form these kind above
+					return 0;
+			}
+		
+
+		}
+
+	}
+
+
+	//Three cards exists
+
+	if((dealtCard.rank >= 10 && dealtCard.rank <= 12) && (card1.rank >= 10 && card1.rank <= 12) && (card2.rank >= 10 && card2.rank <= 12)){
+		if(dealtCard.suit == card1.suit && dealtCard.suit == card2.suit && card1.suit == card2.suit){
+			if(dealtCard.rank != card1.rank && dealtCard.rank != card2.rank && card1.rank != card2.rank){ //JQK Straight Flush
+				return 6;
+			}
+
+		}
+		else{
+			if(dealtCard.rank != card1.rank && dealtCard.rank != card2.rank && card1.rank != card2.rank){ //JQK Straight
+				return 3;
+			}
+			else // Same kind
+				return 2;
+		}
+	}
+	else{ //not JQK
+		var cardArr = new Array();
+			cardArr.push(dealtCard.rank);
+			cardArr.push(card1.rank);
+			cardArr.push(card2.rank);
+			cardArr.sort();
+		if(dealtCard.suit == card1.suit && dealtCard.suit == card2.suit && card1.suit == card2.suit){
+			if(cardArr[2] - cardArr[1] == 1 && cardArr[1] - cardArr[0] == 1){ //Straight Flush
+				return 5;
+			}
+			else //Flush
+				return 4;
+		}
+		else
+			if(cardArr[2] - cardArr[1] == 1 && cardArr[1] - cardArr[0] == 1){ //Straight
+				return 3;
+			}
+			else if(dealtCard.rank - card1.rank == 0 && dealtCard.rank - card2.rank == 0)//same kind
+				return 2;
+			else //Cant form anything
+				return 0;
+	}
+	return 0;
+
+
+
+
+}
+function calValue(value,dealtCard,grid){
+	var currValue = -1;
+	for(var i=0;i<9;i++){
+
+
+		if(!grid[i]){
+			switch(i){
+				case 0: currValue = max(analysisCards(dealtCard,grid[1],grid[2]), analysisCards(dealtCard,grid[3],grid[6]), analysisCards(dealtCard,grid[4],grid[8]),0);break;
+				case 1: currValue = max(analysisCards(dealtCard,grid[0],grid[2]), analysisCards(dealtCard,grid[4],grid[7]),0,0);break;
+				case 2: currValue = max(analysisCards(dealtCard,grid[0],grid[1]), analysisCards(dealtCard,grid[5],grid[8]), analysisCards(dealtCard,grid[4],grid[6]),0);break;
+				case 3: currValue = max(analysisCards(dealtCard,grid[4],grid[5]), analysisCards(dealtCard,grid[0],grid[6]),0,0);break;
+				case 4: currValue = max(analysisCards(dealtCard,grid[3],grid[5]), analysisCards(dealtCard,grid[1],grid[7]), analysisCards(dealtCard,grid[0],grid[8]),analysisCards(dealtCard,grid[2],grid[6]));break;
+				case 5: currValue = max(analysisCards(dealtCard,grid[3],grid[4]), analysisCards(dealtCard,grid[2],grid[8]),0,0);break;
+				case 6: currValue = max(analysisCards(dealtCard,grid[7],grid[8]), analysisCards(dealtCard,grid[0],grid[3]), analysisCards(dealtCard,grid[2],grid[4]),0);break;
+				case 7: currValue = max(analysisCards(dealtCard,grid[6],grid[8]), analysisCards(dealtCard,grid[1],grid[4]),0,0);break;
+				case 8: currValue = max(analysisCards(dealtCard,grid[6],grid[7]), analysisCards(dealtCard,grid[2],grid[5]), analysisCards(dealtCard,grid[0],grid[4]),0);break;
+		
+			}
+		}
+
+		//test
+		//alert("curr:" + i + " currValue:" + currValue);
+		//
+		value[i] = currValue;
+	}
+	return value;
+
+
+	/*for(var i=0;i<9;i++){
+		for (var i = 0; i < map.length; i++) {
+		var cards = [grid[map[i][0]],grid[map[i][1]],grid[map[i][2]]];
+		
+		if (royalFlush(cards)) hands[i] = new Hand(ROYAL_FLUSH,-1);
+		else if (straightFlush(cards)) hands[i] = new Hand(STRAIGHT_FLUSH,grid[map[i][0]].suit);
+		else if (flush(cards)) hands[i] = new Hand(FLUSH,grid[map[i][0]].suit);
+		else if (straight(cards)) hands[i] = new Hand(STRAIGHT,-1);
+		else if (threeOfAKind(cards)) hands[i] = new Hand(THREE_OF_A_KIND,-1);
+		else if (pair(cards)) hands[i] = new Hand(PAIR,-1);
+		else hands[i] = null;
+		}
+	}*/
+	
+
+}
+function calMaxValue(value){
+	var highest = value[0];
+	for(var i=1;i<9;i++){
+		if(value[i] > highest){
+			highest = value[i];
+		}
+	}
+	return highest;
+}
+function checkValue(dealtCards,grid){
+	var valueArr = new Array();
+	var value = [0,0,0,0,0,0,0,0,0];
+	for(var i=0;i<6;i++){
+		if(dealtCards[i].suit != SPECIAL_SUIT){
+			value = calValue(value,dealtCards[i],grid);
+		}
+		valueArr.push(value);
+
+
+		//test
+		/*
+		var temp = "";
+		for(var j=0;j<9;j++){
+			temp = temp + value[i] + " ";
+		}
+		alert(i+ ":  " + temp);
+		//
+		*/
+		value = [0,0,0,0,0,0,0,0,0];
+	}
+	return valueArr;
 }
 
 function conservative(){
